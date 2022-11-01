e8272e9ca0480c0639a3748a0c5ef89ceda9ca02
Sergey Malyunov <FatalRR@yandex.ru>
2022-11-01 18:37:26 +0700

    fix: ArrayList to LinkedList, изменил метод add

--- src/ru/yandex/practicum/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/manager/InMemoryHistoryManager.java
@@ -3,21 +3,20 @@ package ru.yandex.practicum.manager;
 import ru.yandex.practicum.model.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int TASK_LIMIT = 10;
-    private final List<Task> taskHistory = new ArrayList<>();
+    private final List<Task> taskHistory = new LinkedList<>();
 
     @Override
     public void add(Task task) {
         if (task != null) {
-            if (taskHistory.size() >= TASK_LIMIT) {
+            if (taskHistory.size() == TASK_LIMIT) {
                 taskHistory.remove(0);
-                taskHistory.add(task);
-            } else {
-                taskHistory.add(task);
             }
+            taskHistory.add(task);
         } else {
             System.out.println(PrintNot.NOT_TASK);
         }
