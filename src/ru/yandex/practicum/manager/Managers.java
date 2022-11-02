4f824c30204cb1ed3d6f60f7512bdf56340200c6
Sergey Malyunov <FatalRR@yandex.ru>
2022-11-02 15:12:23 +0700

    fix:вернул метод getDefaultHistory()

--- src/ru/yandex/practicum/manager/Managers.java
+++ src/ru/yandex/practicum/manager/Managers.java
@@ -3,6 +3,10 @@ package ru.yandex.practicum.manager;
 public class Managers {
 
     public static TasksManager getDefault() {
-        return new InMemoryTaskManager(new InMemoryHistoryManager());
+        return new InMemoryTaskManager(getDefaultHistory());
+    }
+
+    public static HistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
     }
 }
