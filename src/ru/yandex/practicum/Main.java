bf06c08fec0253a740ff9ec51a95a40f5ca84f8f
Sergey Malyunov <FatalRR@yandex.ru>
2022-12-10 13:02:28 +0700

    тесты для ТЗ 6

--- src/ru/yandex/practicum/Main.java
+++ src/ru/yandex/practicum/Main.java
@@ -1,57 +1,52 @@
 package ru.yandex.practicum;
 
-import ru.yandex.practicum.manager.Managers;
-import ru.yandex.practicum.model.tasks.Epic;
-import ru.yandex.practicum.model.Status;
-import ru.yandex.practicum.model.tasks.Subtask;
+import ru.yandex.practicum.manager.FileBackedTasksManager;
 import ru.yandex.practicum.model.tasks.Task;
-import ru.yandex.practicum.manager.TasksManager;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        TasksManager tasksManager = Managers.getDefault();
+//        System.out.println("Запись в файл");
+//        Path path = Path.of("save.csv");
+//        File file = new File(String.valueOf(path));
+//
+//        FileBackedTasksManager fileBackedTasksManager = new FileBackedTasksManager(file);
+//
+//        fileBackedTasksManager.createTask(new Task("новая задача 1", "описание задачи 1", Status.NEW));
+//        fileBackedTasksManager.createTask(new Task("новая задача 2", "описание задачи 2", Status.NEW));
+//
+//        fileBackedTasksManager.createEpic(new Epic("новый эпик 1", "описания эпик 1", Status.NEW));
+//        fileBackedTasksManager.createEpic(new Epic("новый эпик 2", "описания эпик 2", Status.NEW));
+//
+//        fileBackedTasksManager.createSubtask(new Subtask("подзадача 1-1", "описание подзадачи 1-1", 3, Status.IN_PROGRESS));
+//        fileBackedTasksManager.createSubtask(new Subtask("подзадача 1-2", "описание подзадачи 1-2", 3, Status.IN_PROGRESS));
+//      //---------------------------------------------
+//        System.out.println("Получение подзадач по эпику");
+//        System.out.println("Эпик по id " + fileBackedTasksManager.getEpicById(3) + "\n");
+//        System.out.println("Эпик по id " + fileBackedTasksManager.getEpicById(4) + "\n");
+//        System.out.println("Эпик по id " + fileBackedTasksManager.getSubtaskById(5) + "\n");
+//        List<Task> history = fileBackedTasksManager.getHistory();
+//        System.out.println("История");
+//        System.out.println(history);
+        //---------------------------------------------
+        System.out.println("Чтение из файла");
 
-//---------------------------------------------
-        System.out.println("Тесты создание");
-        tasksManager.createEpic(new Epic("новый эпик 1", "описания эпик 1", Status.NEW));
-        tasksManager.createEpic(new Epic("новый эпик 2", "описания эпик 2", Status.NEW));
-        tasksManager.createEpic(new Epic("новый эпик 3", "описания эпик 3", Status.NEW));
+        Path path2 = Path.of("save.csv");
+        File file2 = new File(String.valueOf(path2));
 
-        tasksManager.createSubtask(new Subtask("подзадача 1-1", "описание подзадачи 1-1", 1, Status.IN_PROGRESS));
-        tasksManager.createSubtask(new Subtask("подзадача 1-2", "описание подзадачи 1-2", 1, Status.NEW));
-        tasksManager.createSubtask(new Subtask("подзадача 1-3", "описание подзадачи 1-3", 1, Status.IN_PROGRESS));
-        tasksManager.createSubtask(new Subtask("подзадача 2-1", "описание подзадачи 1-1", 2, Status.IN_PROGRESS));
-        tasksManager.createSubtask(new Subtask("подзадача 2-2", "описание подзадачи 1-2", 2, Status.NEW));
-        tasksManager.createSubtask(new Subtask("подзадача 2-3", "описание подзадачи 1-2", 2, Status.NEW));
+        FileBackedTasksManager fileBackedTasksManager2 = new FileBackedTasksManager(file2);
+        fileBackedTasksManager2.loadFromFile();
+        List<Task> history2 = fileBackedTasksManager2.getHistory();
 
-        System.out.println("Все задачи: " + tasksManager.getAllTasks() + "\n" +
-                "Все эпик задачи: " + tasksManager.getAllEpic() + "\n" +
-                "Все подзадачи эпиков: " + tasksManager.getAllSubtasks() + "\n");
-//---------------------------------------------
-        System.out.println("Получение подзадач по эпику");
-        System.out.println("Подзадачи по эпику " + tasksManager.getAllSubtasksByEpic(1) + "\n");
-        System.out.println("Подзадачи по эпику " + tasksManager.getAllSubtasksByEpic(1) + "\n");
-        System.out.println("Подзадачи по эпику " + tasksManager.getAllSubtasksByEpic(2) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getEpicById(1) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getEpicById(2) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getEpicById(1) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getEpicById(3) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getEpicById(1) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getEpicById(2) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getSubtaskById(5) + "\n");
-        System.out.println("Эпик по id " + tasksManager.getSubtaskById(5) + "\n");
-        List<Task> history = tasksManager.getHistory();
-//---------------------------------------------
         System.out.println("История");
-        System.out.println(history);
-//---------------------------------------------
-        System.out.println("Удаление из истории просмотров");
-        tasksManager.remove(2);
-        tasksManager.remove(3);
-        List<Task> history2 = tasksManager.getHistory();
         System.out.println(history2);
+        System.out.println("Все задачи: " + fileBackedTasksManager2.getAllTasks() + "\n" +
+                "Все эпик задачи: " + fileBackedTasksManager2.getAllEpic() + "\n" +
+                "Все подзадачи эпиков: " + fileBackedTasksManager2.getAllSubtasks() + "\n");
     }
 }
